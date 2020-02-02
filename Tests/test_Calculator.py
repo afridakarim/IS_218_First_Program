7c7a27fe7af31f1d8b2442f7e36b582ff02e418e
afridakarim <ak2577@njit.edu>
2020-02-01 20:04:21 -0500

    addition

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -3,7 +3,15 @@ from Calculator.Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
+        calculator = Calculator()
         self.assertIsInstance(self.calculator, Calculator)
+
+    def addition_test(self):
+        calculator = Calculator()
+        result = calculator.add(2,2)
+        self.assertEqual(4,result)
+
+    
 
 
 
